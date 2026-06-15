c1bde0fa48c9cfd7d9724b6a45cbb963fc99f1b6
Harikrishna <iamharikrishna@icloud.com>
2026-06-15 15:41:53 +0530

    import fixture in runner

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import {test} from '../src/fixtures/base_fixture';
 
 test('sample test', async ({ page }) => {
   await page.goto('https://playwright.dev');
